ab38ed064c6b98ae3e46a203fb060a3f67124b46
Win-Desktop <mr.doe082@gmail.com>
2022-08-10 14:55:47 +0900

    ㅁㅁ

--- src/main/webapp/resources/js/search/modal.js
+++ src/main/webapp/resources/js/search/modal.js
@@ -31,7 +31,7 @@ document.querySelector('.input_search.show_placeholder_on_focus').addEventListen
                             <p data-v-db6f0438="" class="suggest_title"> ${result.searchedBrandList[i].englishName}
                                 <span data-v-db6f0438="" class="suggest_type">BRAND</span>
                             </p>
-                            <a data-v-db6f0438="" href="/product/list?shoe_brand=${result.searchedBrandList[i].identityNumber}" class="more_link">
+                            <a data-v-db6f0438="" href="/product/list??pageNo=1&qty=12&shoe_brand=${result.searchedBrandList[i].identityNumber}" class="more_link">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
