22088578364e269a98ceb00b847cb79402373a7b
debugger <parapet-origami.0o@icloud.com>
2022-08-11 14:35:59 +0900

    ProductVO th_img

--- src/main/java/com/shoekream/www/domain/productVO/ProductVO.java
+++ src/main/java/com/shoekream/www/domain/productVO/ProductVO.java
@@ -7,6 +7,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 
+@Getter
+@Setter
+@ToString
 @Alias("productVO")
 public class ProductVO {
     private int pno;
@@ -18,66 +21,6 @@ public class ProductVO {
     private int retailPrice;
     private int category;
     private int brand;
-    
-	public int getPno() {
-		return pno;
-	}
-	public void setPno(int pno) {
-		this.pno = pno;
-	}
-	public String getModel() {
-		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	public String geteName() {
-		return eName;
-	}
-	public void seteName(String eName) {
-		this.eName = eName;
-	}
-	public String getkName() {
-		return kName;
-	}
-	public void setkName(String kName) {
-		this.kName = kName;
-	}
-	public int getReadCount() {
-		return readCount;
-	}
-	public void setReadCount(int readCount) {
-		this.readCount = readCount;
-	}
-	public String getRegAt() {
-		return regAt;
-	}
-	public void setRegAt(String regAt) {
-		this.regAt = regAt;
-	}
-	public int getRetailPrice() {
-		return retailPrice;
-	}
-	public void setRetailPrice(int retailPrice) {
-		this.retailPrice = retailPrice;
-	}
-	public int getCategory() {
-		return category;
-	}
-	public void setCategory(int category) {
-		this.category = category;
-	}
-	public int getBrand() {
-		return brand;
-	}
-	public void setBrand(int brand) {
-		this.brand = brand;
-	}
-	@Override
-	public String toString() {
-		return "ProductVO [pno=" + pno + ", model=" + model + ", eName=" + eName + ", kName=" + kName + ", readCount="
-				+ readCount + ", regAt=" + regAt + ", retailPrice=" + retailPrice + ", category=" + category
-				+ ", brand=" + brand + "]";
-	}
-	
+	private String th_img;
+
 }
