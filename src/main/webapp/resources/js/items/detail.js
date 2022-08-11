1dfbb1d081cfa0fd2bd5a4b9b45b012ff06644a8
kimgeunhoe <ghkim1031@naver.com>
2022-08-11 22:01:09 +0900

    kgh추가

--- src/main/webapp/resources/js/items/detail.js
+++ src/main/webapp/resources/js/items/detail.js
@@ -1,3 +1,5 @@
+const pno = document.getElementById('pno').innerText;
+
 document.querySelector('.sizeBtn').addEventListener('click', () => {
     document.querySelector('.layer_detail_size_select').style.display = "block";
 });
@@ -6,3 +8,27 @@ document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.layer_detail_size_select').style.display = "none";
 });
 
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('select_link') || e.target.classList.contains('size') || e.target.classList.contains('price')){
+        e.target.closest('ul').querySelector('.active').classList.remove('active');
+        e.target.closest('li').classList.add('active');
+        document.querySelector('.sizeValue').innerText = e.target.closest('button').querySelector('.size').innerText;
+        let buyPrice = e.target.closest('button').querySelector('.price').innerText;
+        document.querySelector('.buy').querySelector('.num').innerText = buyPrice=='구매입찰' ? '-\u00a0': buyPrice;
+        recentPriceSellPice(pno, e.target.closest('li').dataset.size).then(result => {
+            document.getElementById('recentPriceValue').innerText = result.recentPrice!=null ? result.recentPrice : '-\u00a0';
+            document.querySelector('.sell').querySelector('.num').innerText = result.maxSellPrice!=null ? result.maxSellPrice : '-\u00a0';
+        });
+        document.querySelector('.layer_detail_size_select').style.display = "none";
+    }
+});
+
+async function recentPriceSellPice(pno, size) {
+    try {
+      const resp = await fetch('/items/' + pno + '/' + size);
+      const rp = await resp.json();
+      return await rp;
+    } catch (error) {
+      console.log(error);
+    }
+}
