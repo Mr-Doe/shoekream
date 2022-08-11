7354f6d2b3b550da0bda9016f33c1f70dba921f5
debugger <parapet-origami.0o@icloud.com>
2022-08-11 14:51:41 +0900

    product 게터세터 복윈

--- src/main/java/com/shoekream/www/domain/productVO/ProductVO.java
+++ src/main/java/com/shoekream/www/domain/productVO/ProductVO.java
@@ -7,9 +7,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 
-@Getter
-@Setter
-@ToString
 @Alias("productVO")
 public class ProductVO {
     private int pno;
@@ -21,6 +18,76 @@ public class ProductVO {
     private int retailPrice;
     private int category;
     private int brand;
-	private String thImg;
+
+    private String thImg;
+
+    public int getThImg() {
+        return pno;
+    }
+    public void setThImg(String thImg) {
+        this.thImg = thImg;
+    }
+
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public String getkName() {
+		return kName;
+	}
+	public void setkName(String kName) {
+		this.kName = kName;
+	}
+	public int getReadCount() {
+		return readCount;
+	}
+	public void setReadCount(int readCount) {
+		this.readCount = readCount;
+	}
+	public String getRegAt() {
+		return regAt;
+	}
+	public void setRegAt(String regAt) {
+		this.regAt = regAt;
+	}
+	public int getRetailPrice() {
+		return retailPrice;
+	}
+	public void setRetailPrice(int retailPrice) {
+		this.retailPrice = retailPrice;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public int getBrand() {
+		return brand;
+	}
+	public void setBrand(int brand) {
+		this.brand = brand;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductVO [pno=" + pno + ", model=" + model + ", eName=" + eName + ", kName=" + kName + ", readCount="
+				+ readCount + ", regAt=" + regAt + ", retailPrice=" + retailPrice + ", category=" + category
+				+ ", brand=" + brand + "]";
+	}
 
 }
